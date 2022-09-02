31299a04483dea52a2469ba16b90d9f1962afbb5
San0330 <sanneupane1024@gmail.com>
2022-09-02 19:25:26 +0545

    remove unwanted files + minor fix with relative path

--- src/typeorm/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.User = void 0;
-var User_1 = require("./User");
-exports.User = User_1.User;
-var entities = [User_1.User];
-exports["default"] = entities;
