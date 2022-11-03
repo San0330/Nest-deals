1bd691f6cdfe4bb729eeeeb288d097d8deca7e9d
San0330 <sanneupane1024@gmail.com>
2022-11-03 21:09:46 +0545

    logout route added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,27 +8,42 @@ import {
     UseInterceptors,
     UseGuards,
     Req,
+    Get,
 } from '@nestjs/common';
 import { IAuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
-import { LocalAuthGuard } from './auth.guard';
+import { AuthenticatedGuard, LocalAuthGuard } from './auth.guard';
 import { Request } from 'express';
 import { Public } from './public.decorator';
 import { UserEntity } from '../typeorm';
 import { Services } from '../utils/constants';
 
-@Public()
 @Controller('auth')
 export class AuthController {
     constructor(
         @Inject(Services.AUTH) private readonly authService: IAuthService,
     ) { }
 
+    @Public()
     @Post('login')
     @UseGuards(LocalAuthGuard)
     @UseInterceptors(ClassSerializerInterceptor)
     async login(@Req() request: Request) {
         return new UserEntity(request.user);
+    }
+
+    @Get('status')
+    @UseGuards(AuthenticatedGuard)
+    @UseInterceptors(ClassSerializerInterceptor)
+    async status(@Req() request: Request) {
+        return new UserEntity(request.user);
+    }
+
+    @Post('logout')
+    @UseGuards(AuthenticatedGuard)
+    async logout(@Req() request: Request) {
+        request.session.destroy(() => { });
+        return {}
     }
 
     private async checkIfUserWithEmailExists(email: string) {
@@ -44,6 +59,7 @@ export class AuthController {
         }
     }
 
+    @Public()
     @Post('register')
     @UseInterceptors(ClassSerializerInterceptor)
     async register(@Body() registerUserDto: RegisterUserDto) {
